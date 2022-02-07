978f7e6486a07c1b00ab21d92e630da74589fe02
NanfackSteve <nanfacksteve7@gmail.com>
2022-02-07 15:27:45 +0100

    Add Lifetime and calculate Ei functions

--- Sequential_deploy.py
+++ Sequential_deploy.py
@@ -39,24 +39,26 @@ class LWSN:
         self.K = K
         self.N = N
         self.T = list()
+        self.Oi = list([0] * K)
         self.n = list([0] * K)
+        self.Ei = list([0] * K)
 
     def sensors_deployment(self, lambda_, alpha, p):
         """Determines the number of sensors of each Virtual Node"""
 
-        O = list([0] * self.K)
-
         for virtual_node in range(0, self.K):
             self.T.append(self.calculate_T(virtual_node, lambda_, p))
 
-        self.n = [1 for _ in range(0, self.K)]  # on ajoute 1 capteur dans chaque Bi
+        self.n = [1 for _ in range(0, self.K)]  # ajoute 1 capteur dans chaq Bi
         remaining_sensors = self.N - self.K  # il reste N - K capteurs
 
-        while remaining_sensors != 0:
+        while remaining_sensors >= 0:
             for virtual_node in range(0, self.K):
-                O[virtual_node] = self.calculate_O(virtual_node, lambda_, alpha, p)
+                self.Oi[virtual_node] = self.calculate_O(
+                    virtual_node, lambda_, alpha, p
+                )
 
-            I = [i for i, o in enumerate(O) if o == max(O)]
+            I = [i for i, o in enumerate(self.Oi) if o == max(self.Oi)]
             i = max(I)
             self.n[i] = self.n[i] + 1
             remaining_sensors = remaining_sensors - 1
@@ -107,15 +109,28 @@ class LWSN:
 
         return Ti_bar + Ri_ps
 
+    def calculate_Ei(self, P, t):
+        """Calculation of average Energy consumed by a sensor,
+        in the Virtual Node Bi per time unit"""
 
-if __name__ == "__main__":
+        for i in range(0, self.K):
+            self.Ei[i] = self.Oi[i] * P * t
 
-    # ------------------ Display Settings -------------------
+    def life_Time(self, delta, Enode):
+        """Calculation of the Lifetime of Network"""
 
-    colors = ["red", "blue", "green", "m", "orange"]
-    symbols = ["o-", "h", "*-", "s", "p"]
+        self.calculate_Ei(0.0619, 0.004096)
+        Emax = max(self.Ei)
+        return (Enode / Emax) * delta
+
+
+def graphic_LWSN_deployment(symbol, color):
+
+    # ------------------ Parametres d'Affichage -------------------
+
     plt.title(
-        "Spatial nodes Distribution with \n K = 10, N = 30  and p = 0.5 ", color="blue"
+        "Spatial nodes Distribution with \n " + "K = 10, N = 30  and p = 0.5 ",
+        color="blue",
     )
     plt.xlabel(" Virtual Node Index ")
     plt.xlim(1, 10)
@@ -123,7 +138,7 @@ if __name__ == "__main__":
     plt.ylim(0, 22)
     plt.ylabel(" Deployed Nodes")
 
-    # ------------------ Deployment based on Alpha --------------
+    # ------------------ Deploiement en fonction de Alpha --------------
 
     alpha_values = [0.0, 0.1, 0.4, 0.6, 1]
 
@@ -134,13 +149,60 @@ if __name__ == "__main__":
         plt.plot(
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             Si,
-            symbols[j],
+            symbol[j],
             label="\u03B1 : {}".format(alpha * 1),
-            color=colors[j],
+            color=color[j],
             lw=1,
             linestyle=linestyles_dict["densely dotted"],
         )
-
     plt.legend()
     plt.grid()
     plt.show()
+
+
+def graphic_LWSN_LifeTime(symbol, color):
+
+    # ------------------ Parametres d'Affichage -------------------
+
+    plt.title(
+        "Impact of node's number on LWSN Lifetime \n "
+        + "K = 10, p = 0.0  and \u03B1 = 0.1 ",
+        color="red",
+    )
+    plt.xlabel(" Number of Deployed nodes ")
+    plt.xlim(10, 200)
+
+    # plt.ylim(0, 5000)
+    plt.ylabel(" Lifetime (Year) ")
+
+    # ------------------ Deploiement en fonction de N --------------
+
+    sensors_deplyed = [i for i in range(10, 220, 20)]
+    LTi = list()
+
+    for N in sensors_deplyed:
+        Si = LWSN(N, 10)
+        Si.sensors_deployment(1, 0.1, 0.5)
+        LTi.append(Si.life_Time(5, 5))
+
+    plt.plot(
+        sensors_deplyed,
+        LTi,
+        symbol,
+        color=color,
+        lw=1,
+        linestyle=linestyles_dict["dashed"],
+    )
+
+    # plt.legend()
+    plt.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    colors = ["red", "blue", "green", "m", "orange", "black"]
+    symbols = ["o-", "h", "*-", "s", "p"]
+
+    graphic_LWSN_deployment(symbols, colors)
+    # graphic_LWSN_LifeTime(symbols[4], colors[2])
