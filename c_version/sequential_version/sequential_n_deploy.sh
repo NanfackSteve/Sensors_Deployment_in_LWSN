fe8a817a3b9f4aa61cdc7bdaa8e94d9ec609ae71
NanfackSteve <nanfacksteve7@gmail.com>
2022-02-20 10:37:05 +0100

    Update all re-organize files

--- c_version/sequential_version/sequential_n_deploy.sh
+++ c_version/sequential_version/sequential_n_deploy.sh
@@ -1,20 +1,20 @@
 #/bin/sh
 
-if [ -e sequential_datas_times.dat ];then
-    rm sequential_datas_times.dat #suppr le fich s'il existe
+if [ -e ./sequential_version/sequential_datas_times.dat ];then
+    rm ./sequential_version/sequential_datas_times.dat #suppr le fich s'il existe
 fi
 
 
 for i in `seq 1 10`
 do
     N=$((i*1000))
-    ./sequential_deploy.exe $N 10 1 0.4 0.5 $i 
+    ./sequential_version/sequential_deploy.exe $N 10 1 0.4 0.5 $i 
     
     #recuperation du temps 
     filename=$(echo "datas_deploy_$i.dat")
     time=$(grep "time" ${filename} | cut -f 3 -d " ") 
-    echo "$N $time" >> sequential_datas_times.dat
-    mv datas_deploy_* ./sequential_datas/
+    echo "$N $time" >> ./sequential_version/sequential_datas_times.dat
+    mv datas_deploy_* ./sequential_version/sequential_datas/
 done
 
 echo "$# - $2"
