5850a7791e151e687eb516f0e41bfbb3cc3abe5c
NanfackSteve <nanfacksteve7@gmail.com>
2022-03-23 20:41:47 +0100

    use modulo to choose which thread will compute O[i]

--- c_version/parallel_version/parallel_deploy.c
+++ c_version/parallel_version/parallel_deploy.c
@@ -10,22 +10,22 @@ typedef struct
     int lambda;
     float alpha;
     float p;
+    int K;
+    int N;
 } ParametersT;
 
-int N, K;
 double *T;
 double *O;
 int *n;
-int threadNbr = 0;
 
-void sensors_deployment(double *exec_time, ParametersT *params);
+void sensors_deployment(double *exec_time, ParametersT *params, double O[], double T[], int n[], int *threadNbr);
 void *calculate_Oi(void *args);
-void save_datas(int number, double time);
+void save_datas(int number, double time, int K);
 void graphic(int number);
 
 int main(int argc, char *argv[])
 {
-    int i = 0, Totalcores = 0, filenumber = 0;
+    int i = 0, Totalcores = 0, filenumber = 0, threadNbr = 0;
     double exec_time;
     ParametersT params;
 
@@ -52,41 +52,43 @@ int main(int argc, char *argv[])
     // }
 
     // Initialisation des Parametres de l'algo de Deploiement
-    N = atoi(argv[1]);
-    K = atoi(argv[2]);
+    params.N = atoi(argv[1]);
+    params.K = atoi(argv[2]);
     params.lambda = atoi(argv[3]);
     params.alpha = atof(argv[4]);
     params.p = atof(argv[5]);
     if (argc == 8)
         filenumber = atoi(argv[7]);
 
-    T = malloc(K * sizeof(double));
-    O = malloc(K * sizeof(double));
-    n = malloc(K * sizeof(int));
+    T = malloc(params.K * sizeof(double));
+    O = malloc(params.K * sizeof(double));
+    n = malloc(params.K * sizeof(int));
 
-    sensors_deployment(&exec_time, &params);
+    sensors_deployment(&exec_time, &params, O, T, n, &threadNbr);
 
     // Affichage
-    printf("\nAfter deployment of N = %d sensors in K = %d Virtual nodes we have:\n", N, K);
-    for (i = 0; i < K; i++)
+    printf("\nAfter deployment of N = %d sensors in K = %d Virtual nodes we have:\n", params.N, params.K);
+    for (i = 0; i < params.K; i++)
         printf(" \nVirt. Node %d \t =    %d sensor(s)\n", i + 1, n[i]);
     printf("\n+-----------------------+\n| Time Exec. = %lf |\n+-----------------------+\n\n", exec_time);
 
-    save_datas(filenumber, exec_time);
+    save_datas(filenumber, exec_time, params.K);
     free(O);
     free(T);
     free(n);
     return 0;
 }
 
-void sensors_deployment(double *exec_time, ParametersT *params)
+void sensors_deployment(double *exec_time, ParametersT *params, double O[], double T[], int n[], int *threadNbr)
 {
-    int i = 0, j = 0, remaining_sensors = 0, indice = 0, lambda;
+    int i = 0, j = 0, remaining_sensors = 0, indice = 0, lambda, N, K, idThread;
     float maxO = 0.0, p;
 
+    N = params->N;
+    K = params->K;
     lambda = params->lambda;
     p = params->p;
-    pthread_t threads[threadNbr];
+    pthread_t threads[*threadNbr];
     clock_t start, end;
 
     // Evaluation du Temps
@@ -106,11 +108,12 @@ void sensors_deployment(double *exec_time, ParametersT *params)
     while (remaining_sensors != 0)
     {
         // Calcul du Nbr d'Oper. Oi de chaq Bi
-        for (i = 0; i < threadNbr; i++)
+        for (i = 0; i < K; i++)
         {
             // Lancement des Threads
+            idThread = i % (*threadNbr);
             params->num = i;
-            pthread_create(&threads[i], NULL, calculate_Oi, (void *)params);
+            pthread_create(&threads[idThread], NULL, calculate_Oi, (void *)params);
             pthread_join(threads[i], NULL);
         }
 
@@ -139,6 +142,7 @@ void sensors_deployment(double *exec_time, ParametersT *params)
 void *calculate_Oi(void *args)
 {
     ParametersT *params = (ParametersT *)args;
+    int K = params->K;
     int virtNode = params->num;
     int lambda = params->lambda;
     float alpha = params->alpha;
@@ -186,7 +190,7 @@ void *calculate_Oi(void *args)
     O[virtNode] = Ti_bar + Ri_ps;
 }
 
-void save_datas(int number, double time)
+void save_datas(int number, double time, int K)
 {
     int i = 0;
 
